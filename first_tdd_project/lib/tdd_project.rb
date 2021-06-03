df1e035cf7752f87dcee6359230b022be94bc0c5
Joseph Kim <jkjoeeekim@live.com>
2021-06-03 14:52:14 -0700

    Add ::two_sum method

--- first_tdd_project/lib/tdd_project.rb
+++ first_tdd_project/lib/tdd_project.rb
@@ -1,7 +1,19 @@
-def my_uniq( arr )
-   new_arr = []
-   arr.each do |ele|
-     (new_arr << ele) unless new_arr.include?(ele)
-   end 
-   new_arr
+def my_uniq(arr)
+  new_arr = []
+  arr.each do |ele|
+    (new_arr << ele) unless new_arr.include?(ele)
+  end
+  new_arr
+end
+
+class Array
+  def two_sum
+    new_arr = []
+    (0...(length - 1)).each do |idx1|
+      ((idx1 + 1)...length).each do |idx2|
+        new_arr << [idx1, idx2] if (self[idx1] + self[idx2]).zero?
+      end
+    end
+    new_arr
+  end
 end
--- first_tdd_project/spec/tdd_project_spec.rb
+++ first_tdd_project/spec/tdd_project_spec.rb
@@ -1,25 +1,37 @@
-require "rspec"
-require "tdd_project"
+require 'rspec'
+require 'tdd_project'
 
-describe "remove_dups" do
-    describe "#my_uniq" do
-        subject(:arr) { [1,2,3,3,4,4] }
-        subject(:arr2) { [1,2,3,2,1,4] }
+describe 'remove_dups' do
+  describe '#my_uniq' do
+    subject(:arr) { [1, 2, 3, 3, 4, 4] }
+    subject(:arr2) { [1, 2, 3, 2, 1, 4] }
 
-        it "takes in an array as argument" do
-            expect { my_uniq(arr) }.not_to raise_error(ArgumentError) 
-        end
-
-        it "should remove duplicates" do
-            expect(my_uniq(arr)).to eq([1,2,3,4])
-            expect(my_uniq(arr2)).to eq([1,2,3,4])
-        end
-
-        it "should return a new array" do
-            expect(my_uniq(arr)).not_to be(arr)
-        end
-
+    it 'takes in an array as argument' do
+      expect { my_uniq }.to raise_error(ArgumentError)
     end
+
+    it 'should remove duplicates' do
+      expect(my_uniq(arr)).to eq([1, 2, 3, 4])
+      expect(my_uniq(arr2)).to eq([1, 2, 3, 4])
+    end
+
+    it 'should return a new array' do
+      expect(my_uniq(arr)).not_to be(arr)
+    end
+  end
 end
 
+describe Array do
+  describe '#two_sum' do
+    subject(:arr) { [-1, 0, 2, -2, 1] }
+    subject(:arr2) { [0, -2, -3, 2, 3] }
 
+    it 'finds pairs of positions where the elements sum to zero' do
+      expect(arr2.two_sum).to eq([[1, 3], [2, 4]])
+    end
+
+    it 'should return pairs in ascending order' do
+      expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+    end
+  end
+end
